da15ca7a39a49b5b7ff2217e218727e0ac1b87be
NanoLoriginal <kyliann.faggian@gmail.com>
2021-11-22 15:50:18 +0100

    je sais ap

--- src/main1.js
+++ src/main1.js
@@ -1,15 +1,37 @@
-let gameConfig = {
-    type: Phaser.AUTO,
-    width: 960,
-    height: 540,
-    backgroundColor: '#ffffff',
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    pixelArt: true,
     parent: 'game',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: new Tableau1()
+    scene: {
+        preload: preload,
+        create: create
+    }
 };
-let game = new Phaser.Game(gameConfig);
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('layer1', 'Characters/boy/boy_style_1/PNG/run/Layer-1.png');
+}
+
+function create ()
+{
+    var marker = this.add.image(100, 100, 'layer1').setAlpha(0.3);
+    var image = this.add.image(100, 100, 'layer1');
+
+    //  flipY will call toggleFlipY on the image whenever it yoyos or repeats
+
+    var tween = this.tweens.add({
+        targets: image,
+        x: 600,
+        ease: 'Power1',
+        duration: 3000,
+        flipY: true,
+        yoyo: true,
+        repeat: -1
+    });
+}
