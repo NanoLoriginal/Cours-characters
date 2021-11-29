ad09d75f1b41ae37090638209115aac7b7f8477b
NanoLoriginal <kyliann.faggian@gmail.com>
2021-11-29 15:31:14 +0100

    je sais ap

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,38 +1,48 @@
-class animation extends Phaser.Scene{
+class Tableau1 extends Phaser.Scene{
     preload(){
 
-        for(let i=1;i<=8;i++) {
-            this.load.image('boy_run' + i, 'png/run/run-' + i + '.png');
-        }
-
+        this.loadFrames('boy_run','Characters/boy/boy_style_1/PNG/run/Layer-',8)
         for(let i=1;i<=10;i++) {
             this.load.image('boy_walk' + i, 'png/walk/walk-' + i + '.png');
         }
-
         for(let i=1;i<=10;i++) {
             this.load.image('boy_idle' + i, 'png/idle/idle2-' + i + '.png');
         }
     }
-
     create(){
         this.run_anim = this.add.sprite(400, 100, 'boy_run1').setOrigin(0,0);
+        console.log(frames)
         this.anims.create({
             key: 'animation_run',
-            frames: [
-                {key:'boy_run1'},
-                {key:'boy_run2'},
-                {key:'boy_run3'},
-                {key:'boy_run4'},
-                {key:'boy_run5'},
-                {key:'boy_run6'},
-                {key:'boy_run7'},
-                {key:'boy_run8'}
-            ],
-            frameRate: 14,
+            frames: this.getFrames("boy_run", 8),
+            frameRate: 12,
             repeat: -1
         });
         this.run_anim.play('animation_run');
         this.run_anim.scale=1
+    }
+
+    /**
+     * Renvoie un tableau d'images
+     * @param {string} prefix Préfixe de la clé (key) à générer
+     * @param {string} url base d'url pour charger le fichier
+     * @param {Number} length combien d'images charger?
+     * @returns {*[]}
+     */
+    loadFrames(prefix,url,length){
+        let frames=[];
+        for (let i=1;i<=length;i++){
+            this.load.image(prefix + i,url+i+'.png')
+        }
+        return frames;
+    }
+
+    getFrames(prefix,length){
+        let frames=[];
+        for (let i=1;i<=length;i++){
+            frames.push({key: prefix+i});
+        }
+        return frames;
     }
 
 }
--- src/main1.js
+++ src/main1.js
@@ -1,37 +1,15 @@
-var config = {
-    type: Phaser.WEBGL,
-    width: 800,
-    height: 600,
-    backgroundColor: '#2d2d2d',
-    pixelArt: true,
+let gameConfig = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 540,
+    backgroundColor: '#ffffff',
     parent: 'game',
-    scene: {
-        preload: preload,
-        create: create
-    }
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: new Tableau1()
 };
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('layer1', 'Characters/boy/boy_style_1/PNG/run/Layer-1.png');
-}
-
-function create ()
-{
-    var marker = this.add.image(100, 100, 'layer1').setAlpha(0.3);
-    var image = this.add.image(100, 100, 'layer1');
-
-    //  flipY will call toggleFlipY on the image whenever it yoyos or repeats
-
-    var tween = this.tweens.add({
-        targets: image,
-        x: 600,
-        ease: 'Power1',
-        duration: 3000,
-        flipY: true,
-        yoyo: true,
-        repeat: -1
-    });
-}
+let game = new Phaser.Game(gameConfig);
